ac55ac005099aec660cf0b1801ebf1502e720cca
Lahiru Karunarathna <64624843+Lahiru-cyber@users.noreply.github.com>
2022-10-17 15:37:04 +0530

    Delete School Management System directory

--- School Management System/src/main/java/com/school/repository/loginRepositroy.java
+++
@@ -1,9 +0,0 @@
-package com.school.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.school.model.login;
-
-public interface loginRepositroy extends JpaRepository<Integer, login>{
-
-}
